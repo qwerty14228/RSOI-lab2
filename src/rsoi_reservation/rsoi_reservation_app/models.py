eb6e6f8f825182d84a1b35fbfaca4d294c2c2dd4
qwerty14228 <shapranmatvey@gmail.com>
2024-11-23 12:53:40 +0300

    Добавил apps и models

--- src/rsoi_reservation/rsoi_reservation_app/models.py
+++ src/rsoi_reservation/rsoi_reservation_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Resarvation(models.Model):
+    reservation_uid = models.UUIDField(verbose_name='Reservation')
+    username = models.CharField(max_length=80)
+    book_uid = models.UUIDField(verbose_name='Book')
+    library_uid = models.UUIDField(verbose_name='Library')
+    status = models.CharField(max_length=20)
+    start_date = models.DateField()
+    till_date = models.DateField()
