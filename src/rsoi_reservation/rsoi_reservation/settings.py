94c6b84c7df75e94d9e7cf3389c40de66ce488ba
qwerty14228 <shapranmatvey@gmail.com>
2024-12-04 22:07:47 +0300

    Начали подключаться к постгресу

--- src/rsoi_reservation/rsoi_reservation/settings.py
+++ src/rsoi_reservation/rsoi_reservation/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'rsoi_reservation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': environ['POSTGRES_USER'],
+        'NAME': environ['POSTGRES_DB'],
+        'PASSWORD': environ['POSTGRES_PASSWORD'],
     }
 }
 
