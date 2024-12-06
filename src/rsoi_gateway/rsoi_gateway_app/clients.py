a586cb88e580bd07e80689d9f6dedbb7446996e1
qwerty14228 <shapranmatvey@gmail.com>
2024-12-06 20:28:35 +0300

    прокинули параметры в ручку получения книг библиотеки

--- src/rsoi_gateway/rsoi_gateway_app/clients.py
+++ src/rsoi_gateway/rsoi_gateway_app/clients.py
@@ -9,3 +9,11 @@ class LibraryClient:
         response = requests.get(f'{self.api_url}/libraries', params={"city": city, "page": page, "size": size})
         data = response.json()
         return {"page": page, "pageSize": size, "totalElements": data["count"], "items": data["results"]}
+
+    def get_library_books(self, library_uid='', page=1, size=10, show_all=False): 
+        params = {'library__library_uid': library_uid, 'page': page, 'size': size}
+        if not show_all:
+            params['available_count__gt'] = 0
+        response = requests.get(f'{self.api_url}/library_books', params=params)
+        data = response.json()
+        return {"page": page, "pageSize": size, "totalElements": data["count"], "items": data["results"]}
--- src/rsoi_gateway/rsoi_gateway_app/views.py
+++ src/rsoi_gateway/rsoi_gateway_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from rest_framework import permissions, viewsets
 
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from rsoi_gateway.settings import SERVICE_URLS
 from rsoi_gateway_app.clients import LibraryClient
@@ -14,7 +15,17 @@ class LibraryViewSet(viewsets.ViewSet):
    client = LibraryClient(SERVICE_URLS['library'])
 
    def list(self, request):
-      return Response(self.client.get_libraries(**request.query_params))
+      qp = request.query_params
+      return Response(self.client.get_libraries(page=int(qp.get('page', 1)), size=int(qp.get('size', 10)), city=qp.get('city', '')))
+   
+   @action(detail=True, methods=['get'], url_name='books', url_path='books')
+   def list_books(self, request, pk=None):
+      qp = request.query_params
+      return Response(self.client.get_library_books(
+         library_uid=pk, page=int(qp.get('page', 1)), 
+         size=int(qp.get('size', 10)),
+         show_all=bool(qp.get('showAll', False))
+      ))
 
 
 def healthcheck_view(request):
