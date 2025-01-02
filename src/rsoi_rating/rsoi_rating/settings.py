0bf2c31f1f0d20cb852f403d25e39638b7414ac5
qwerty14228 <shapranmatvey@gmail.com>
2025-01-02 12:44:33 +0300

    добавили шаг запуска юнит тестов на сервисе рейтинга

--- src/rsoi_rating/rsoi_rating/settings.py
+++ src/rsoi_rating/rsoi_rating/settings.py
@@ -77,16 +77,24 @@ WSGI_APPLICATION = 'rsoi_rating.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': environ['POSTGRES_HOST'],
-        'PORT': 5432,
-        'USER': environ['POSTGRES_USER'],
-        'NAME': environ['POSTGRES_DB'],
-        'PASSWORD': environ['POSTGRES_PASSWORD'],
+if 'RUN_UNIT_TESTS' in environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:'
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': environ['POSTGRES_HOST'],
+            'PORT': 5432,
+            'USER': environ['POSTGRES_USER'],
+            'NAME': environ['POSTGRES_DB'],
+            'PASSWORD': environ['POSTGRES_PASSWORD'],
+        }
+    }
 
 
 # Password validation
@@ -130,8 +138,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ['rsoi_common.auth.RsoiAuthentication'],
-}
+if 'RUN_UNIT_TESTS' in environ:
+    REST_FRAMEWORK = {}
+else:
+    REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': ['rsoi_common.auth.RsoiAuthentication'],
+    }
 
 APPEND_SLASH = False
--- src/rsoi_rating/rsoi_rating_app/tests.py
+++ src/rsoi_rating/rsoi_rating_app/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rsoi_rating_app.models import Rating
+
+class RatingTestCase(TestCase):
+    def test_rating_creation(self):
+        rating = Rating.objects.create(username="test-user", stars=50)
+        self.assertEqual(rating.username, "test-user")
+        self.assertEqual(rating.stars, 50)
